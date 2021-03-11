7182a4d8f5176efbe80ab5ee32888e25f6fa2210
javahyang <javahyang@gmail.com>
2021-03-11 21:40:46 +0900

    mustache로 index 페이지 생성

---
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,12 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index"; // 머스테치스타터 덕분에 앞경로(src/main/resources/templates/)와 파일확장자(.mustache) 자동으로 붙어서 실행됨
+    }
+}
