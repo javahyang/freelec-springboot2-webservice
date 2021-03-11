eeb5fec635382cc5a906f0d33021a33ea841021a
javahyang <javahyang@gmail.com>
2021-03-11 22:50:30 +0900

    feat : post 삭제 api 연동

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -40,4 +40,10 @@ public class PostsService {
     public List<PostsListResponseDto> findAllDesc() {
         return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        postsRepository.delete(posts); // JpaRepository 에서 이미 delete 메소드 지원. deleteById 메소드 이용하면 id로 삭제 가능
+    }
 }
