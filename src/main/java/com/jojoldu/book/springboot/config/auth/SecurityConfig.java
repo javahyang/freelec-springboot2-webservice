0286d28d318e4cb9c4d2005ba2041a113d2dc598
javahyang <javahyang@gmail.com>
2021-03-21 19:29:40 +0900

    feat : 무중단 배포를 위한 API 추가

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .csrf().disable().headers().frameOptions().disable() // h2-console 화면 사용하기위해 옵션 disable
             .and()
                 .authorizeRequests() // URL 별 권한관리 설정 옵션의 시작점
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll() // 전체 열람 권한
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll() // 전체 열람 권한
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // USER 권한 가진 사람만 가능
                 .anyRequest().authenticated() // 설정된 값 이외 나머지 URL 들에 인증용(로그인)된 사용자만 허
             .and()
